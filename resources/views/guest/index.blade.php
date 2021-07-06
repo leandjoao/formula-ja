a5f4273452629ff362d4fcc093bb754476c181ac
João Vitor <leand.joao@outlook.com.br>
2021-07-05 21:18:02 -0300

    banner update

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,21 +1,18 @@
 @extends('layouts.guest')
 @section('content')
     <section class="banner">
-        <div class="banner-background">
-            <div class="banner-background-primary">
-                <div class="banner-background-primary-text">
-                    <h4>Lorem Ipsum</h4>
-                    <h2>Lorem Ipsum dolor sit amet consectetur adipiscing elit</h2>
-                    <p>
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                        nulla
-                        pariatur.
-                    </p>
-                    <a href="">Envie sua receita!</a>
-                </div>
+        <div class="banner-container">
+            <div class="banner-container-text">
+                <h4>Lorem Ipsum</h4>
+                <h2>Lorem Ipsum dolor sit amet consectetur adipiscing elit</h2>
+                <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                    nulla
+                    pariatur.
+                </p>
+                <a href="">Envie sua receita!</a>
             </div>
-            <div class="banner-background-secondary"></div>
         </div>
 
         <div class="how-it-works">
