515c0a3e40896e5e95d10a44729627be79bc647f
João Vitor <leand.joao@outlook.com.br>
2021-07-16 10:57:26 -0300

    blog front-end

--- resources/views/guest/blog.blade.php
+++ resources/views/guest/blog.blade.php
@@ -6,35 +6,35 @@
     </div>
     <div class="blog-section-content">
         <section class="blog-content">
-            <div class="no-posts">
-                <h2>Infelizmente, não temos nenhum post 🙁</h2>
-            </div>
-            {{--
-            <div class="blog-content-posts">
-                @for($i = 0; $i < 9; $i++)
-                <div class="post">
-                    <div class="post-image">
-                        <img src="http://placehold.it/277x200" alt="">
-                    </div>
-                    <div class="post-text">
-                        <h4>Título</h4>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, risus quis maximus ultricies, sapien purus elementum justo, at vestibulum nunc est eget lacus. Donec egestas sagittis nunc, vel tristique sapien commodo vitae.
-                        </p>
-                        <div class="post-text-footer">
-                            <small>{{ Carbon\Carbon::now()->add($i, 'day')->diffForHumans() }}</small>
-                            <a href="">Ler Mais <i class="fas fa-long-arrow-alt-right"></i></a>
-                        </div>
-                    </div>
+            @if(empty($posts['data']))
+                <div class="no-posts">
+                    <h2>Infelizmente, não temos nenhum post 🙁</h2>
                 </div>
-                @endfor
-            </div>
-            --}}
+            @else
+                <div class="blog-content-posts">
+                    @for($i = 0; $i < 9; $i++)
+                        <div class="post">
+                            <div class="post-image">
+                                <img src="http://placehold.it/277x200" alt="">
+                            </div>
+                            <div class="post-text">
+                                <h4>Título</h4>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque convallis, risus quis maximus ultricies, sapien purus elementum justo, at vestibulum nunc est eget lacus. Donec egestas sagittis nunc, vel tristique sapien commodo vitae.
+                                </p>
+                                <div class="post-text-footer">
+                                    <small>{{ Carbon\Carbon::now()->add($i, 'day')->diffForHumans() }}</small>
+                                    <a href="">Ler Mais <i class="fas fa-long-arrow-alt-right"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    @endfor
+                </div>
+            @endif
         </section>
         <aside class="blog-aside">
             <div class="blog-aside-blocks">
 
-                {{--
                 <div class="block block-search">
                     <h5>Buscar</h5>
                     <form action="{{ route('blog.search') }}" method="POST" class="block-search-form">
@@ -44,13 +44,12 @@
                         </div>
                     </form>
                 </div>
-                --}}
 
                 <div class="block block-instagram">
                     <h5>Instagram</h5>
                     <div class="block-instagram-images">
                         @for($i = 0; $i < 6; $i++)
-                        <img src="http://placehold.it/76x76" alt="" />
+                            <img src="http://placehold.it/76x76" alt="" />
                         @endfor
                     </div>
                 </div>
@@ -63,37 +62,37 @@
                         <a href=""><i class="fab fa-youtube"></i></a>
                     </div>
                 </div>
+                @if(!empty($posts['data']))
+                    <div class="block block-categories">
+                        <h5>Categorias</h5>
+                        <ul class="block-categories-list">
+                            @for($i = 0; $i < 6; $i++)
+                                <li>
+                                    <a href="">Categoria</a>
+                                    <p>(14)</p>
+                                </li>
+                            @endfor
+                        </ul>
+                    </div>
 
-                {{--
-                <div class="block block-categories">
-                    <h5>Categorias</h5>
-                    <ul class="block-categories-list">
-                        @for($i = 0; $i < 6; $i++)
-                        <li>
-                            <a href="">Categoria</a>
-                            <p>(14)</p>
-                        </li>
-                        @endfor
-                    </ul>
-                </div>
+                    <div class="block block-feed">
+                        <h5>Últimos Posts</h5>
+                        <ul class="block-feed-posts">
+                            @for($i = 0; $i < 4; $i++)
+                                <li>
+                                    <a href="">
+                                        <img src="http://placehold.it/70x70" alt="">
+                                        <span>
+                                            <p>Titulo</p>
+                                            <small>{{ Carbon\Carbon::now()->add($i, 'day')->diffForHumans() }}</small>
+                                        </span>
+                                    </a>
+                                </li>
+                            @endfor
+                        </ul>
+                    </div>
+                @endif
 
-                <div class="block block-feed">
-                    <h5>Últimos Posts</h5>
-                    <ul class="block-feed-posts">
-                        @for($i = 0; $i < 4; $i++)
-                        <li>
-                            <a href="">
-                                <img src="http://placehold.it/70x70" alt="">
-                                <span>
-                                    <p>Titulo</p>
-                                    <small>{{ Carbon\Carbon::now()->add($i, 'day')->diffForHumans() }}</small>
-                                </span>
-                            </a>
-                        </li>
-                        @endfor
-                    </ul>
-                </div>
-                --}}
             </div>
         </aside>
     </div>
