d2d8e968bea9dd5b92b3bd0ff5888effb8264304
João Vitor <leand.joao@outlook.com.br>
2021-08-18 16:25:15 -0300

    permissio fix

--- resources/views/admin/budgets/inner.blade.php
+++ resources/views/admin/budgets/inner.blade.php
@@ -39,14 +39,16 @@
                 </table>
             </div>
 
-            @if(Auth::user()->access_level === '2')
+            @if(Auth::user()->access_level === '1' || Auth::user()->access_level === '2')
                 <div class="budget-answered">
                     <div class="budget-answered-title">
                         <h3>Orçamento Respondido</h3>
+                        @if(Auth::user()->access_level === '2')
                         <div class="btn-group">
                             <div class="accept"><i class="fa fa-check"></i> Aceitar</div>
                             <div class="revoke"><i class="fa fa-times"></i> Recusar</div>
                         </div>
+                        @endif
                     </div>
                     <ul class="budget-answered-list">
                         <li>
@@ -80,14 +82,16 @@
                     </div>
                 </div>
             @endif
-            @if(Auth::user()->access_level === '1' || Auth::user()->access_level === '3')
+            @if(Auth::user()->access_level === '3')
             <div class="budget-answer">
                 <div class="budget-answer-title">
                     <h3>Responder orçamento</h3>
+                    @if(Auth::user()->access_level === '3')
                     <div class="btn-group">
                         <div class="add"><i class="fa fa-plus"></i> Adicionar</div>
                         <div class="remove"><i class="fa fa-minus"></i> Remover</div>
                     </div>
+                    @endif
                 </div>
                 <div class="budget-answer-list">
                     <form action="" class="form">
