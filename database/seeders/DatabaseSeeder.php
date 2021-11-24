8cc4cfd4584b65c4f4ef6432d697ad013ba87f7b
João Vitor <leand.joao@outlook.com.br>
2021-11-24 03:14:10 -0300

    Removed Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             Config::class,
             PostCategorySeeder::class,
             PostSeeder::class,
-            PartnersSeeder::class
+            // PartnersSeeder::class
         ]);
     }
 }
