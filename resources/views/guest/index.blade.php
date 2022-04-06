5123fd009a1c3657895776014f9cc275ace83f25
João Vitor <38146531+leandjoao@users.noreply.github.com>
2022-04-06 00:07:44 -0300

    Update index.blade.php

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -66,9 +66,11 @@
                 <div class="posts-item-title">
                     <p>{{$post['title']}}</p>
                 </div>
+                {{--
                 <div class="posts-item-text">
                     {!! Str::limit($post['content'], 130, '...') !!}
                 </div>
+                --}}
                 <div class="posts-item-footer">
                     <p>{{ Carbon\Carbon::parse($post['created_at'])->diffForHumans() }}</p>
                     <a href="{{route('guest.blog.inner', [\Str::slug($post['category']['label']), $post['slug']])}}">Ler Mais <i class="fas fa-long-arrow-alt-right"></i></a>
